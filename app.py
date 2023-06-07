10ce19b192029a1eab405538ac69750f9db1dae0
milanitommaso <milani.tommaso2004@gmail.com>
2023-06-07 15:55:44 +0000

    created flask app

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
 # app.py
+
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route("/github")
+def github():
+    return redirect("https://github.com/milanitommaso")
+
+@app.route("/linkedin")
+def linkedin():
+    return redirect("https://www.linkedin.com/in/milani-tommaso/")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
